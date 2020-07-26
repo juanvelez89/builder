3b33dd446084aa67a395fb6cd7b082b37d0edaaf
jdaniel.vg@aguapanelab.co <juan.velezw@gmail.com>
2020-07-25 20:31:58 -0500

    primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("primer commit!");
+        }
+    }
+}
